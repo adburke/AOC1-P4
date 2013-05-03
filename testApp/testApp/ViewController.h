f93b827b39384ff87ec66938436fd7e3daf3c8a8
adburke <adburke@fullsail.edu>
2013-05-02 20:50:25 -0500

    Work

--- testApp/testApp/ViewController.h
+++ testApp/testApp/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+  UITextField *userNameInput;  
+}
 @end
--- testApp/testApp/ViewController.m
+++ testApp/testApp/ViewController.m
@@ -24,6 +24,13 @@
         [self.view addSubview:inputLabel];
     }
     
+    userNameInput = [[UITextField alloc] initWithFrame:CGRectMake(290.0f, 300.0f, 300.0f, 40.0f)];
+    if (userNameInput)
+    {
+        userNameInput.borderStyle = UITextBorderStyleRoundedRect;
+        userNameInput.font = [UIFont systemFontOfSize:20.0f];
+        [self.view addSubview:userNameInput];
+    }
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
